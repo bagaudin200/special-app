276c51380a7dec65e6cc3bd54d4347957d40398f
Bagaudin <bagaudin200@mail.ru>
2021-11-13 18:33:27 +0300

    фит

---
+++ питон.py
@@ -0,0 +1,12 @@
+number = int(input('Введите число: '))
+summ = 0
+
+while number != 0:
+    last_num = number % 10
+    summ += last_num
+    if last_num == 5:
+        print('Обнаружен разрыв')
+        break
+
+    print('Текущая сумма цифр:', summ)
+    print('\nИтоговая сумма цифр', summ)
